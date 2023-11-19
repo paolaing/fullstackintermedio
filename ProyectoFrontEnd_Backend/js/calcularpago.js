43cff0ab8a637506c53be7a8465a45d838f7c452
paolaing <paolaing14@gmail.com>
2023-11-18 21:53:03 -0500

    segundo commit

--- ProyectoFrontEnd_Backend/js/calcularpago.js
+++ ProyectoFrontEnd_Backend/js/calcularpago.js
@@ -2,31 +2,42 @@ function mensaje() {
     alert('DEBE REGISTRARSE, GRACIAS');
 }
 
-function myFunction() {
+var escogerdivisa = document.getElementById("moneda_escoger");
+escogerdivisa.addEventListener("change", function () {
+    convertirdinero();
+    
+});
+
+
+var divisacomprar = document.getElementById("divisacomprar");
+divisacomprar.addEventListener("input", function () {
+    convertirdinero();
+
+});
+
+function convertirdinero() {
+
+  
+
     var x = document.getElementById("moneda_escoger").value;
     console.log(x)
 
     if (document.getElementById("moneda_escoger").value == "1") {
-        var w = 4567;
+        var valordolar = 4350;
         if (document.getElementById("divisacomprar")) {
             // renamed x to z
             var z = document.getElementById("divisacomprar").value;
-            var wc = w * z;
-
-
-            // not .value but .textContent
-
-            document.getElementById("costo").value = wc;
-
-
-        }
+            var pagototal = valordolar * z;
+                        // not .value but .textContent
+            document.getElementById("costo").value = pagototal;
+            console.log("costo")
+                    }
     }
-
-        else {
-                var w = 500;
+            else {
+                var valoreuro = 4560;
                 if (document.getElementById("divisacomprar")) {
                 var z = document.getElementById("divisacomprar").value;
-                var wc = w * z;
+                var wc = valoreuro * z;
 
 
                 // not .value but .textContent
